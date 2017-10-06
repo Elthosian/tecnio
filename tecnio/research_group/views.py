358fe071127d338e34d15f792c462ecf4cf6dd1e
Alberto Mario Ceballos Arroyo <rzks95@gmail.com>
2017-10-05 21:23:08 -0500

    khkh

--- tecnio/research_group/views.py
+++ tecnio/research_group/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Article
+
 
 def home(request):
-    return HttpResponse('Hello, World!')
+    articles = Article.objects.all()
+    return render(request, 'home.html', {'articles': articles})
+
