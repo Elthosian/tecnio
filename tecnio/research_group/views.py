432dc92aa986386ea086b885b8f6b64455404c6c
Alberto Mario Ceballos Arroyo <rzks95@gmail.com>
2017-10-05 22:28:45 -0500

    Cambios noche 4 de octubre 2017

--- tecnio/research_group/views.py
+++ tecnio/research_group/views.py
@@ -7,3 +7,10 @@ def home(request):
     articles = Article.objects.all()
     return render(request, 'home.html', {'articles': articles})
 
+
+def about(request):
+    return render(request, 'about.html')
+
+
+def index(request):
+    return render(request, 'index.html')
--- tecnio/tecnio/urls.py
+++ tecnio/tecnio/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 from research_group import views
 
 urlpatterns = [
-    url(r'^$', views.home, name='home'),
+    url(r'^$', views.index, name='index'),
+    url(r'^about/', views.about, name='about'),
     url(r'^admin/', admin.site.urls),
+    #url(r'^index/', views.index, name='index'),
 ]
